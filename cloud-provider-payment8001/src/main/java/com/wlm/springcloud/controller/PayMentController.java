796180abdc8fc8fdea54644a3115436310c1cea9
wuleiming <822507481@qq.com>
2021-02-03 17:46:11 +0800

    springcloud--修改查询代码

--- cloud-provider-payment8001/src/main/java/com/wlm/springcloud/controller/PayMentController.java
+++ cloud-provider-payment8001/src/main/java/com/wlm/springcloud/controller/PayMentController.java
@@ -4,20 +4,17 @@ import com.wlm.springcloud.entity.CommonResult;
 import com.wlm.springcloud.entity.PayMent;
 import com.wlm.springcloud.service.IPayMentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "payment")
+@RequestMapping(value = "/payment")
 public class PayMentController {
 
     @Autowired
     private IPayMentService iPayMentService;
 
     @PostMapping(value = "/save")
-    public CommonResult create(PayMent payMent) {
+    public CommonResult create(@RequestBody PayMent payMent) {
         int res = iPayMentService.save(payMent);
         if (res > 0) {
             return new CommonResult(200, "成功", res);
@@ -25,7 +22,7 @@ public class PayMentController {
         return new CommonResult(500, "失败");
     }
 
-    @PostMapping(value = "/get/{id}")
+    @GetMapping(value = "/get/{id}")
     public CommonResult get(@PathVariable("id") Long id) {
         PayMent payMent = iPayMentService.getPayMentById(id);
         if (payMent != null) {
--- cloud-provider-payment8001/src/main/java/com/wlm/springcloud/entity/PayMent.java
+++ cloud-provider-payment8001/src/main/java/com/wlm/springcloud/entity/PayMent.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class PayMent implements Serializable {
 
+
     private Long id;
 
     private String serial;
